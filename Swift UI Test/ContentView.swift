232401a8981dcabd7d3552c2e628fba9d10e5492
quentintheastronaut <quan260402@gmail.com>
2020-06-25 22:59:23 +0700

    Initial Commit

---
+++ Swift UI Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Test
+//
+//  Created by Đặng Nhật Quân on 6/25/20.
+//  Copyright © 2020 Đặng Nhật Quân. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
